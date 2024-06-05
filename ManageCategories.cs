388008c6ef71b874822c292697dbaea12d4cc52a
ngocnghia0611 <ngocnghia0611@gmail.com>
2024-06-05 22:17:07 +0700

    add file

---
+++ ManageCategories.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace ManageCategoriesApp
+{
+    public record Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+    public class ManageCategories
+    {
+        SqlConnection connection;
+        SqlCommand command;
+        String ConnectionString = "Server=(local);uid=sa;pwd=06112002;database=MyStore;TrustServerCertificate=True";
+        public List<Category> GetCategories()
+        {
+            List<Category> categories = new List<Category>();
+            connection = new SqlConnection(ConnectionString);
+            string SQL = "select CategoryID, CategoryName from Categories";
+            command = new SqlCommand(SQL, connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                if (reader.HasRows == true)
+                {
+                    while (reader.Read())
+                    {
+                        categories.Add(new Category
+                        {
+                            CategoryID = reader.GetInt32("CategoryID"),
+                            CategoryName = reader.GetString("CategoryName")
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return categories;
+        }
+
+        public void InsertCategory(Category category)
+        {
+            connection = new SqlConnection(ConnectionString);
+            command = new SqlCommand("Insert Categories value @(CategoryName)", connection);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            connection = new SqlConnection(ConnectionString);
+            string SQL = "Update Categories set CategoryName=@CategoryName Where CategoryID=@CategoryID";
+            command = new SqlCommand(SQL, connection);
+            command.Parameters.AddWithValue("CategoryID", category.CategoryID);
+            command.Parameters.AddWithValue("CategoryName", category.CategoryName);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            connection = new SqlConnection(ConnectionString);
+            string SQL = "Delete Categories Where CategoryID=@CategoryID";
+            command = new SqlCommand(SQL, connection);
+            command.Parameters.AddWithValue("CategoryID", category.CategoryID);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
